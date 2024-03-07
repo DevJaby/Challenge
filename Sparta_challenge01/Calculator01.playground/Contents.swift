b91ef2b98f4594453ef84c34ed968fb9e4c7863e
JABY <devjaby@gmail.com>
2024-03-07 21:43:15 +0900

    030724

--- Sparta_challenge01/Calculator01.playground/Contents.swift
+++ Sparta_challenge01/Calculator01.playground/Contents.swift
@@ -1,25 +1,41 @@
-func calc(a: Int, b: Int, op: String) -> String {
-  switch op {
-    case "+":
-      return "\(a) + \(b) = \(a + b)"
-    case "-":
-      return "\(a) - \(b) = \(a - b)"
-    case "*":
-      return "\(a) * \(b) = \(a * b)"
-    case "/":
-      if b == 0 {
-        return "숫자 아님"
-      } else {
-        return "\(a) / \(b) = \(a / b)"
-      }
-    default:
-      return "사용할 수 없는 연산자입니다."
-  }
+// MARK: 계산기 만들기
+
+
+// Lv.1
+
+class Calculator {
+    var a:Int = 0
+    var b:Int = 0
+    var operation: String = ""
+
+    func calculate(a: Int, b: Int, op: String) -> String {
+        switch operation {
+        case "+":
+            return "\(a) + \(b) = \(a + b)"
+        case "-":
+            return "\(a) - \(b) = \(a - b)"
+        case "*":
+            return "\(a) * \(b) = \(a * b)"
+        case "/":
+            if b == 0 {
+                return "숫자 아님"
+            } else {
+                return "\(a) / \(b) = \(a / b)"
+            }
+        default:
+            return "사용할 수 없는 연산자입니다."
+        }
+    }
 }
 
-//출력 예시
-let a = 10
-let op = "/"
-let b = 2
+// 인스턴스 생성 및 변수 할당
+let calculator = Calculator()
 
-print(calc(a: a, b: b, op: op))
+//숫자와 연산자 입력
+calculator.a = 10
+calculator.b = 2
+calculator.operation = "/"
+
+
+let result = calculator.calculate(a: 10, b: 2, op: "/")
+print(result)
--- Sparta_challenge01/Calculator02.playground/Contents.swift
+++ Sparta_challenge01/Calculator02.playground/Contents.swift
@@ -1,20 +1,25 @@
 class Calculator {
-    var num1: Double = 0
-    var num2: Double = 0
-    var operation: String = ""
+    var num1: Double = 0 // 첫번째 숫자 선언
+    var num2: Double = 0 // 두번째 숫자 선언
+    var operation: String = "" // 연산자
     
+    // 덧셈 연산 함수
     func add(num1: Double, num2: Double) -> Double {
         return num1 + num2
     }
+    // 빼기 연산 함수
     func subtract(num1: Double, num2: Double) -> Double {
         return num1 - num2
     }
+    // 곱하기 연산 함수
     func multiply(num1: Double, num2: Double) -> Double {
         return num1 * num2
     }
+    // 나누기 연산 함수
     func divide(num1: Double, num2: Double) -> Double? {
         return num2 != 0 ? num1 / num2 : nil
     }
+    // 계산 실행 함수
     func calculate() -> Double {
         var result: Double = 0
         switch operation {
@@ -35,11 +40,11 @@ class Calculator {
 
 let calculator = Calculator()
 
-calculator.num1 = 110
-calculator.operation = "/"
-calculator.num2 = 0
+calculator.num1 = 110 // 첫번째 수
+calculator.operation = "/" // 연산자
+calculator.num2 = 0 // 두번째 수
 
-let result = calculator.calculate()
+let result = calculator
 print(result)
 
 // 계산기 만들기
